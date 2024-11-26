c7b55370735c4754b6ac643064b1378e9571f704
Linnea Lehmann Molander <chalottelinnea@hotmail.com>
2024-11-26 16:38:51 +0100

    Creating JSON server

---
+++ script.js
@@ -0,0 +1,10 @@
+/*fetch("https://fakestoreapi.com/products/")
+    .then(response => response.json())
+    .then((data) => {
+        
+        for(let i = 0; i < data.length; i++) {
+            const category = data.id[i].category;
+            document.getElementById("categoryContainer").textContent = 
+
+        }
+    })*/
