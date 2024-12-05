ec09fef5c5b83ccc8d5eb2e5184efa8e4b56f46e
Linnea Lehmann Molander <chalottelinnea@hotmail.com>
2024-12-05 15:53:36 +0100

    Changes in script, creating functions for all btns

--- script.js
+++ script.js
@@ -3,21 +3,48 @@
 (async function () {
     const resp = await fetch("https://fakestoreapi.com/products");
     const data = await resp.json();
+    
+    const clothesBtn = document.getElementById("clothes");
 
+    function showClothes() {
+        document.getElementById("productCard").innerHTML = "";
 
+        for(let i = 0; i < data.length; i++){
+            const product = data[i];
+            const category = product.category;
+            if(category.toLowerCase().includes("clothing")) {
+                const productElem = `
+                <div class="product">
+                    <img class="productImg" src=${product.image}>
+                    <p>${product.title}</p>
+                    <p>${product.price} $</p>
+                </div>`;
+                document.getElementById("productCard").innerHTML += productElem;
+            }
+        }
 
-    for (let i = 0; i < data.length; i++) {
-        const product = data[i]
-        const imgUrl = product.image;
-
-        const productElem = `
-            <div class="product">
-                <img class="productImg" src=${imgUrl}>
-                <p>${product.title}</p>
-                <p>${product.price} $</p>
-            </div>`;
-
-        document.getElementById("productCard").innerHTML += productElem;
     }
+    clothesBtn.addEventListener("click", showClothes);
+
+    const jeweleryBtn = document.getElementById("jewelery");
+
+    function showJewelery() {
+        document.getElementById("productCard").innerHTML = "";
+
+        for(let i = 0; i < data.length; i++) {
+            const product = data[i];
+            const category = product.category;
+            if(category.toLowerCase().includes("jewelery")) {
+                const productElem = `
+                <div class="product">
+                    <img class="productImg" src=${product.image}>
+                    <p>${product.title}</p>
+                    <p>${product.price} $</p>
+                </div>`;
+                document.getElementById("productCard").innerHTML += productElem;
+            }
+        }
+    }
+    jeweleryBtn.addEventListener("click", showJewelery);
 
 })();
