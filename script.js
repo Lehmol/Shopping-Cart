2350e7c257e4e5e7ca1bbd24330f7763b7b8bbce
Linnea Lehmann Molander <chalottelinnea@hotmail.com>
2024-12-05 11:26:53 +0100

    Changes in script, making less code

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const currentProductIndex = 0;
     const resp = await fetch("https://fakestoreapi.com/products");
     const data = await resp.json();
     const product = data[currentProductIndex];
-    document.getElementById("title").innerText = product.title;
-    document.getElementById("img").src = product.image;
-    document.getElementById("price").innerText = product.price;
-    document.getElementById("description").innerText = product.description;
+    const imgUrl = data[currentProductIndex].image;
 
-    const rating = product.rating;
+    const productElem = `
+        <img src=${imgUrl}>
+        <p>${product.title}</p>
+        <p>${product.price}</p>
+        <p>${product.description}</p>`;
+        document.getElementById("productCard").innerHTML = productElem;
 
 })();
