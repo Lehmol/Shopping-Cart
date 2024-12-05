6ca6996fa4081a612a4c935b36d02e0d6d5d0d34
Linnea Lehmann Molander <chalottelinnea@hotmail.com>
2024-12-05 13:30:24 +0100

    Changes in styling

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 // Using IIFE (Immediatley invoked function expression)
-const currentProductIndex = 0;
 
 (async function () {
     const resp = await fetch("https://fakestoreapi.com/products");
     const data = await resp.json();
-    const product = data[currentProductIndex];
-    const imgUrl = data[currentProductIndex].image;
 
-    const productElem = `
-        <img src=${imgUrl}>
-        <p>${product.title}</p>
-        <p>${product.price}</p>
-        <p>${product.description}</p>`;
-        document.getElementById("productCard").innerHTML = productElem;
+
+
+    for (let i = 0; i < data.length; i++) {
+        const product = data[i]
+        const imgUrl = product.image;
+
+        const productElem = `
+            <div class="product">
+                <img class="productImg" src=${imgUrl}>
+                <p>${product.title}</p>
+                <p>${product.price} $</p>
+            </div>`;
+
+        document.getElementById("productCard").innerHTML += productElem;
+    }
 
 })();
